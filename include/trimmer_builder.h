9491411f706207aeb85c8f8982e3bd879fd8b38e
John Lawson <john@jwlawson.co.uk>
2015-01-22 14:42:14 +0000

    Adds copyright headers.
    Also adds short file description to those which did not have one.

--- include/trimmer_builder.h
+++ include/trimmer_builder.h
@@ -1,5 +1,23 @@
 /**
  * trimmer_builder.h
+ * Copyright 2014-2015 John Lawson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Builder for all Trimmers. TrimmerBuilder::function() sets which of the
+ * trimmers to build, and the various options are set using the builder
+ * functions.
  */
 #pragma once
 
